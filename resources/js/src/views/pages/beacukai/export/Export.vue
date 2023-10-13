5186a9ebdca2688a75d131dc28e2860d6b21badd
adekiruz <nrmuhamad@gmail.com>
2023-10-13 17:12:31 +0700

    add page export import

---
+++ resources/js/src/views/pages/beacukai/export/Export.vue
@@ -0,0 +1,83 @@
+<template>
+  <b-row class="match-height">
+    <b-col
+      cols="12"
+    >
+      <p class="mb-2">
+        <span>You can easily create reuseable chart components. </span>
+        <b-link
+          href="https://vue-chartjs.org/"
+          target="_blank"
+        >
+          vue-chartjs
+        </b-link>
+        <span> is a wrapper for Chart.js. Read full documnetation </span>
+        <b-link
+          href="https://vue-chartjs.org/guide/"
+          target="_blank"
+        >
+          here
+        </b-link>
+      </p>
+    </b-col>
+    <b-col md="6">
+      <chartjs-bar-chart />
+    </b-col>
+    <b-col md="6">
+      <chartjs-horizontal-bar-chart />
+    </b-col>
+    <b-col cols="12">
+      <chartjs-line-chart />
+    </b-col>
+    <b-col md="6">
+      <chartjs-radar-chart />
+    </b-col>
+    <b-col md="6">
+      <chartjs-polar-area-chart />
+    </b-col>
+    <b-col cols="12">
+      <chartjs-bubble-chart />
+    </b-col>
+    <b-col lg="4">
+      <chartjs-doughnut-chart />
+    </b-col>
+    <b-col lg="8">
+      <chartjs-scatter-chart />
+    </b-col>
+    <b-col cols="12">
+      <chartjs-line-area-chart />
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import { BRow, BCol, BLink } from 'bootstrap-vue'
+
+import ChartjsBarChart from '../../../charts-and-maps/charts/chartjs/ChartjsBarChart.vue'
+import ChartjsLineChart from '../../../charts-and-maps/charts/chartjs/ChartjsLineChart.vue'
+import ChartjsLineAreaChart from '../../../charts-and-maps/charts/chartjs/ChartjsLineAreaChart.vue'
+import ChartjsRadarChart from '../../../charts-and-maps/charts/chartjs/ChartjsRadarChart.vue'
+import ChartjsPolarAreaChart from '../../../charts-and-maps/charts/chartjs/ChartjsPolarAreaChart.vue'
+import ChartjsDoughnutChart from '../../../charts-and-maps/charts/chartjs/ChartjsDoughnutChart.vue'
+import ChartjsHorizontalBarChart from '../../../charts-and-maps/charts/chartjs/ChartjsHorizontalBarChart.vue'
+import ChartjsBubbleChart from '../../../charts-and-maps/charts/chartjs/ChartjsBubbleChart.vue'
+import ChartjsScatterChart from '../../../charts-and-maps/charts/chartjs/ChartjsScatterChart.vue'
+
+export default {
+  components: {
+    BRow,
+    BCol,
+    BLink,
+
+    ChartjsBarChart,
+    ChartjsLineChart,
+    ChartjsLineAreaChart,
+    ChartjsRadarChart,
+    ChartjsPolarAreaChart,
+    ChartjsDoughnutChart,
+    ChartjsHorizontalBarChart,
+    ChartjsScatterChart,
+    ChartjsBubbleChart,
+  },
+}
+</script>
---
+++ resources/js/src/views/pages/beacukai/import/Import.vue
@@ -0,0 +1,78 @@
+<template>
+    <b-row class="match-height">
+        <b-col md="4"><chartjs-line-chart /></b-col>
+        <b-col md="4">
+            <b-card no-body>
+                <b-card-header>
+                    <div class="d-flex flex-column justify-content-center align-items-center w-100">
+                        <b-card-title>Komposisi Jalur PIB</b-card-title>
+                        <span>Th-2023</span>
+                    </div>
+                </b-card-header>
+
+                <!-- chart -->
+                <b-card-body>
+                    <chartjs-component-pie-chart :data="pieChartData" :options="pieChartOptions" />
+                </b-card-body>
+            </b-card>
+        </b-col>
+        <b-col md="4"><chartjs-line-chart /></b-col>
+    </b-row>
+</template>
+  
+<script>
+import { BRow, BCol, BLink, BCard, BCardHeader, BCardBody, BCardTitle } from 'bootstrap-vue';
+import flatPickr from 'vue-flatpickr-component';
+
+import ChartjsComponentPieChart from '../../../charts-and-maps/charts/chartjs/charts-components/ChartjsComponentPieChart.vue';
+
+import ChartjsLineChart from '../../../charts-and-maps/charts/chartjs/ChartjsLineChart.vue';
+
+export default {
+    data() {
+        return {
+            dataCount: 5,
+            pieChartData: {
+                labels: [ 'Merah', 'Kuning', 'Hijau' ],
+                datasets: [
+                    {
+                        label: 'Dataset 1',
+                        data: [ 7.7, 0.0, 92.3 ],
+                        backgroundColor: Object.values({
+                            red: 'red',
+                            yellow: 'yellow',
+                            green: 'green',
+                        }),
+                    }
+                ]
+            },
+            pieChartOptions: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Chart.js Pie Chart'
+                    }
+                }
+            }
+        };
+    },
+    components: {
+        BRow,
+        BCol,
+        BLink,
+        BCard,
+        BCardHeader,
+        BCardBody,
+        BCardTitle,
+        flatPickr,
+
+        ChartjsLineChart,
+        ChartjsComponentPieChart,
+    },
+};
+</script>
+  
